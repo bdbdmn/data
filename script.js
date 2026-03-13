9e68afef1b8d4014f3cc25d7273633c9ce43c2e2
Brian D <45578714+daftquest@users.noreply.github.com>
2026-03-13 09:21:41 -0600

    updated styles???

--- script.js
+++ script.js
@@ -5,18 +5,37 @@ fetch("https://sheetdb.io/api/v1/gtq7w6uzj647w?sheet=movies")
     const postChrisGrid = document.getElementById("post-chris-grid");
     for (let i = data.length - 1; i > 71; i--) {
         let filmCard = document.createElement("div");
-        filmCard.classList.add("film-card");
         let poster = document.createElement("img");
-        poster.src = data[i].poster_url;
         let date = document.createElement("p");
-        date.textContent = data[i].date_watched;
         let choiceOf = document.createElement("p");
-        choiceOf.textContent = data[i].chosen_by;
+        let textDiv = document.createElement("div");
+        filmCard.classList.add("film-card");
         poster.classList.add("card-poster");
         date.classList.add("date");
         choiceOf.classList.add("choice-of");
-        let textDiv = document.createElement("div");
         textDiv.classList.add("card-text");
+        if (data[i].movie_id == "NP") {
+          poster.src = "nextNorm.webp";
+          date.textContent = "Next pick is from:";
+          choiceOf.textContent = data[i].chosen_by;
+        } else if (data[i].movie_id == "Null") {
+          poster.src = "norm.webp";
+          date.textContent = "Soon...from";
+          choiceOf.textContent = data[i].chosen_by;
+        } else {
+          poster.src = data[i].poster_url;
+          date.textContent = data[i].date_watched;
+          choiceOf.textContent = data[i].chosen_by;
+        }
+        if (choiceOf.textContent == "Jacob") {
+          choiceOf.classList.add("jacob");
+        } else if (choiceOf.textContent == "Sam") {
+          choiceOf.classList.add("sam");
+        } else if (choiceOf.textContent == "Brian") {
+          choiceOf.classList.add("brian");
+        } else if (choiceOf.textContent == "Chris") {
+          choiceOf.classList.add("chris");
+        }
         textDiv.appendChild(date);
         textDiv.appendChild(choiceOf);
         filmCard.appendChild(poster);
